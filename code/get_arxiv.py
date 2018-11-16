c000d32e2a42bdc591f574734ad2c5ede17ca192
Sam Kowash <kowash@uw.edu>
2018-11-15 19:44:58 -0800

    Some comments in getter/parser

--- code/get_arxiv.py
+++ code/get_arxiv.py
@@ -3,30 +3,41 @@ import numpy as np
 from oaipmh.client import Client
 from oaipmh.metadata import MetadataRegistry, oai_dc_reader
 import string
+import datetime
 
 # Use with caution; this will pull down ALL papers in the specified arXiv set
 
-URL = 'http://export.arxiv.org/oai2'
+URL = 'http://export.arxiv.org/oai2' # define repository URL
 
 registry = MetadataRegistry()
-registry.registerReader('oai_dc',oai_dc_reader)
-client = Client(URL, registry)
-client.updateGranularity()
-
+registry.registerReader('oai_dc',oai_dc_reader) # define metadata reader
+client = Client(URL, registry) # create harvester
+print("Updating granularity if necessary.")
+#client.updateGranularity() # if selective harvesting necessary
+print("Client created.")
 
 
 set_ = 'physics:hep-th'
 metadataPrefix = 'oai_dc'
 
-records = client.listRecords(metadataPrefix=metadataPrefix, set=set_)
 
+
+print("Making request.")
+# get records from repository; probably a long time (~80k papers)
+records = client.listRecords(metadataPrefix=metadataPrefix, set=set_)
+print("Harvesting complete.")
+
+# extract abstracts from records
 abstracts = np.array([record[1].getField('description')[0] for record in records])
 
 np.save('../data/arxiv_raw_abstracts.npy',abstracts)
+print("Raw abstracts saved.")
 
 parsed = []
 
 
+
+# parse into lower-case, punctuation-free strings
 for i in range(len(abstracts)):
     abstract = ' '.join(abstracts[i].lower().split())
     table = str.maketrans({key: None for key in string.punctuation})
@@ -35,7 +46,7 @@ for i in range(len(abstracts)):
 
 
 np.save('../data/arxiv_parsed.npy',np.array(parsed))
-
+print("Parsed abstracts saved, exiting.")
 
 
 
