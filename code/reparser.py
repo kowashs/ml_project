3a373cea9ef0f0a1a7db0789bd2839467826f806
Sam Kowash <kowash@uw.edu>
2018-12-05 18:42:56 -0800

    arXiv fetcher?

---
+++ code/reparser.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import numpy as np
+import string
+import sys
+
+filename = sys.argv[1]
+
+data = np.load(filename)
+
+
+reparsed = []
+
+
+# parse into lower-case, punctuation-free strings
+for i in range(len(abstracts)):
+    abstract = ' '.join(abstracts[i].lower().split().split('-'))
+    table = str.maketrans({key: None for key in string.punctuation})
+    abstract = abstract.translate(table)
+    parsed.append(abstract)
+
+
+np.save('../data/arxiv_parsed.npy',np.array(parsed))
+print("rsed abstracts saved, exiting.")
