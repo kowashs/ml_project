57848bfe61156abe24ed8095e5b6fe69128e846d
Sam Kowash <kowash@uw.edu>
2018-12-07 09:22:33 -0800

    Fixed tf-idf type bugs)

--- code/tf_idf.py
+++ code/tf_idf.py
@@ -2,8 +2,8 @@
 import numpy as np
 import api_get_abstracts as getter
 
-arxiv_tot = getter.get_stored_arxiv(2000)
-snarxiv_tot = getter.get_snarxiv(2000)
+arxiv_tot = getter.get_stored_arxiv(1000)
+snarxiv_tot = getter.get_snarxiv(1000)
 
 arxiv_train = arxiv_tot[:500]
 snarxiv_train = snarxiv_tot[:500]
@@ -17,20 +17,31 @@ def idf(vocab,arxiv,snarxiv): #assumes abstracts pre-parsed, but in (N,1) array
     N_arxiv = len(arxiv)
     N_snarxiv = len(snarxiv)
     N_tot = N_arxiv + N_snarxiv
-    tot = np.concatenate((arxiv,snarxiv))
-    dfs = np.zeros_like(vocab,dtype=float)
+
+    dfs = np.zeros(vocab.shape)
+    dfs_arx = np.zeros(vocab.shape)
+    dfs_snarx = np.zeros(vocab.shape)
+
 
     for i,word in enumerate(vocab):
-        for abstract in tot:
+        for abstract in arxiv:
             if word in abstract:
                 dfs[i] += 1
-
-    return np.log10(N_tot/dfs)
+                dfs_arx += 1
+        for abstract in snarxiv:
+            if word in abstract
+                dfs[i] += 1
+                dfs_snarx +=1
+    
+    idfs = np.log10((N_tot+1)/(dfs+1))
+    idfs_arx = np.log10((N_arxiv+1)/(dfs_arx+1))
+    idfs_snarx = np.log10((
+    return np.log10((N_tot+1)/(dfs+1)),
 
 
 def tf(vocab, abstract):
-    counts = np.zeros_like(vocab,dtype=float)
-    tfs = np.zeros_like(vocab,dtype=float)
+    counts = np.zeros(vocab.shape)
+    tfs = np.zero(vocab.shape)
 
     for i,word in enumerate(vocab):
         if word in abstract:
